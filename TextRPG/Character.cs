ef6139597e1da2a80b72b4c314ce50ce1bedc547
Marek Holub <holubm@mg.intra>
2018-10-04 15:27:14 +0200

    Add project files.

---
+++ TextRPG/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    interface Character
+    {
+        string Name { get; }
+        int Hp { get; }
+        int Dmg { get; }
+
+        void Meelee();
+        void Ranged();
+        void Block();
+
+        void Equip(int plusDmg);
+    }
+}
---
+++ TextRPG/Warrior.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    class Warrior : Character
+    {
+        public string Name { private set; get; }
+
+        public int Hp { private set; get; }
+
+        public int Dmg => throw new NotImplementedException();
+
+        public void Block()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Equip(int plusDmg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Meelee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Ranged()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
